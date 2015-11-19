71371e1f76ff59a6be416c909ef285477c13ca9c
Sara and Nina <nina_singh@hotmail.com>
2015-11-18 19:32:07 -0800

    second commit

--- 01-list-primes/list_primes.py
+++ 01-list-primes/list_primes.py
@@ -1,7 +1,12 @@
-primes = [1,2]
+primes = [2,3]
 #primes = primes + 5
 primes = primes + [5]
 #primes.append(7,11)
-primes.append([7,11])
-primes.pop()
+#primes.append([7,11])
+#primes.pop()
+primes = primes + [7,11]
+#extend=[13,17]
+#primes.extend(extend)
+primes.extend([13])
+primes.extend([17])
 print primes
